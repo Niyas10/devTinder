caad25f5c32d99b196f439359174a66f38061791
Niyas10 <niyazmuhammad688@gmail.com>
2025-02-12 14:06:31 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -2,9 +2,20 @@ const express = require('express')
 
 const app = express()
 
-app.use("/",(req,res)=>{
-    res.send("hello world ")
+app.use("/hello/2",(req,res)=>{
+    res.send("/hello/2")
 })
+
+app.use("/hello",(req,res)=>{
+    res.send("hello ")
+})
+
+app.use("/",(req,res)=>{
+    res.send("/")
+})
+
+
+
 
 
 
