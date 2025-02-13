c5002f5d4c8cfaf8c7686eb26eafc586f2e90671
Niyas10 <niyazmuhammad688@gmail.com>
2025-02-13 13:39:04 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,23 +1,14 @@
 const express = require('express')
-
 const app = express()
 
-app.use("/hello/2",(req,res)=>{
-    res.send("/hello/2")
+
+app.use("/user",(req,res,next)=>{
+    console.log("Handling the route user..")
+    next()
+},(req,res,next)=>{
+    console.log("1st route")
+    res.send('first route')
 })
-
-app.use("/hello",(req,res)=>{
-    res.send("hello ")
-})
-
-app.use("/",(req,res)=>{
-    res.send("/")
-})
-
-
-
-
-
 
 
 app.listen(3000,()=>{
