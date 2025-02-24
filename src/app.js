a93d19e7a24dedcfe3b07367d7846ac5c93cc44c
Niyas10 <niyazmuhammad688@gmail.com>
2025-02-24 10:16:57 +0530

    dynamic data signin

--- src/app.js
+++ src/app.js
@@ -1,63 +1,32 @@
-// const express = require("express");
-// const connectDB = require("./config/database");
-// const app = express();
-// const User = require("./models/user")
+const express = require("express");
+const app = express();
+const connectDb = require("./config/database");
+const User = require("./models/user");
 
 
-// app.post("/signup",async (req,res)=>{
-//     const user = new User({
-//         firstName:'Niyas',
-//         lastName:"P",
-//         emailId:'niyas@gmail.com',
-//         password:"niyas123"
-//     })
+app.use(express.json())
 
-//     await user.save()
-//     res.send("user added succesfully")
-// })
+app.post("/signup", async (req, res) => {
+
+  const user = new User(req.body)
 
 
 
+  try {
+    await user.save();
+    res.send("succesfully user added ");
+  } catch (err) {
+    res.status(400).send("error saving the user." + err.message);
+  }
+});
 
-
-
-
-const express = require("express")
-const app = express()
-const connectDb = require("./config/database")
-const User = require("./models/user")
-
-app.post("/signup",async(req,res)=>{
-    const user =  new User (
-      {
-        firstName:"Niyas",
-        lastName:"p",
-        emailId:"test@gmail.com",
-        password:"123456789"
-      }
-    )
-
-
-    try{
-      await user.save()
-      res.send("succesfully user added ")
-    }catch(err){
-   res.status(400).send("error saving the user."+err.message)
-    }
- 
-})
-
-
-
-
-connectDb().then(()=>{
-  console.log("Database Connected Successfully")
-  app.listen(3000,()=>{
-    console.log("server running successfully")
+connectDb()
+  .then(() => {
+    console.log("Database Connected Successfully");
+    app.listen(3000, () => {
+      console.log("server running successfully");
+    });
   })
-
-}).catch((err)=>{
-  console.log("Database not connected")
-})
-
-
+  .catch((err) => {
+    console.log("Database not connected");
+  });
