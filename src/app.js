0cf9ad368a426f09cc8e62687857e9713ce715dc
Niyas10 <niyazmuhammad688@gmail.com>
2025-02-24 12:38:54 +0530

    get apis

--- src/app.js
+++ src/app.js
@@ -7,11 +7,7 @@ const User = require("./models/user");
 app.use(express.json())
 
 app.post("/signup", async (req, res) => {
-
   const user = new User(req.body)
-
-
-
   try {
     await user.save();
     res.send("succesfully user added ");
@@ -19,6 +15,36 @@ app.post("/signup", async (req, res) => {
     res.status(400).send("error saving the user." + err.message);
   }
 });
+
+app.get("/user",async(req,res)=>{
+  const userEmail = req.body.emailId
+
+  try{
+    const users = await User.find({emailId:userEmail})
+    if(users.length===0){
+      res.status(400).send("User Not Found")
+    }else{
+      res.send(users)
+    }
+
+  }catch(err){
+     res.status(400).send("SOmething went wrong")
+  }
+})
+
+app.get("/feed", async (req,res)=>{
+  try{
+    const users = await User.find({})
+    console.log(users)
+    res.send(users)
+
+  }catch(err){
+    res.status(400).send("Something went wrong")
+  }
+})
+
+
+
 
 connectDb()
   .then(() => {
