5f2e9a9de38628e722a377e7e18daacfad0eaae9
Niyas10 <niyazmuhammad688@gmail.com>
2025-03-02 17:48:47 +0530

    hashing

--- src/app.js
+++ src/app.js
@@ -2,14 +2,22 @@ const express = require("express");
 const app = express();
 const connectDb = require("./config/database");
 const User = require("./models/user");
+const { validateSignUpData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
-
-app.use(express.json())
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  const user = new User(req.body)
-  
   try {
+    validateSignUpData(req);
+    const { password, firstName, lastName, emailId } = req.body;
+    const passwordHas = await bcrypt.hash(password, 10);
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHas,
+    });
     await user.save();
     res.send("succesfully user added ");
   } catch (err) {
@@ -17,77 +25,88 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-app.get("/user",async(req,res)=>{
-  const userEmail = req.body.emailId
-
-  try{
-    const users = await User.find({emailId:userEmail})
-    if(users.length===0){
-      res.status(400).send("User Not Found")
-    }else{
-      res.send(users)
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid Credential");
     }
 
-  }catch(err){
-     res.status(400).send("Something went wrong")
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (isPasswordValid) {
+      res.send("Login is successfull");
+    } else {
+      throw new Error("Invalid Credential");
+    }
+  } catch (err) {
+    res.status(400).send("Error :" + err.message);
   }
-})
+});
 
-app.get("/feed", async (req,res)=>{
-  try{
-    const users = await User.find({})
-    res.send(users)
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
 
-  }catch(err){
-    res.status(400).send("Something went wrong")
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(400).send("User Not Found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
   }
-})
+});
 
-app.delete("/user",async (req,res)=>{
-  const userId = req.body.userId
-
-  try{
-    const user = await User.findByIdAndDelete(userId)
-  
-    res.send("user delete succesfully")
-
-
-  }catch(err){
-    res.status(400).send("Something went wrong")
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
   }
-})
+});
 
-app.patch("/user",async (req,res)=>{
-  const userId = req.body.userId
-  const data = req.body
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
 
-  try{
+  try {
+    const user = await User.findByIdAndDelete(userId);
 
-    await User.findByIdAndUpdate({_id:userId},data,{
-      returnDocument:"after",
-      runValidators:true
-    })
-    res.send("user updated successfully")
-
-  }catch(err){
-    res.status(400).send("something went wrong")
+    res.send("user delete succesfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
   }
+});
 
-})
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
 
+  try {
+    const ALLOWED_UPDATE = ["photoUrl", "about", "gender", "age", "skills"];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATE.includes(k)
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Update Not Allowed");
+    }
 
+    if (data?.skills.length > 10) {
+      throw new Error("SKill cannot be morethan 10");
+    }
 
-
-
-
-
-
-
-
-
-
-
-
+    await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    res.send("user updated successfully");
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
 
 connectDb()
   .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,51 +1,71 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    minLength:2,
-    maxLength:50
-  },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-    required: true,
-    lowercase:true,
-    unique: true,
-    trim:true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-    min:18
-  },
-  gender: {
-    type: String,
-    validate(value){
-        if(!["male","female","others"].includes(value)){
-            throw new Error("Gender data is not valid")
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address   " + value);
         }
-    }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("enter a strong password   " + value);
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://img.freepik.com/premium-vector/user-profile-icon-flat-style-member-avatar-vector-illustration-isolated-background-human-permission-sign-business-concept_157943-15752.jpg",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid photo URL  " + value);
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "This is a defulat about of the user ..!",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  photoUrl : {
-    type: String,
-    default:"https://img.freepik.com/premium-vector/user-profile-icon-flat-style-member-avatar-vector-illustration-isolated-background-human-permission-sign-business-concept_157943-15752.jpg"
-  },
-  about : {
-    type:String,
-    default : "This is a defulat about of the user ..!"
-  },
-  skills : {
-      type:[String],
+  {
+    timestamps: true,
   }
-},{
-    timestamps:true,
-});
+);
 
 module.exports = mongoose.model("User", userSchema);
